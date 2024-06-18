7d83793d26bef6c05a0e696691caccebea31c67b
Ong Hin Journ <onghinjourn@gmail.com>
2024-06-19 01:11:57 +0800

    feat: :sparkles: adds tailwind and vue components and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-import alpinejs from "@astrojs/alpinejs";
+
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://chunithm.sg",
-  integrations: [alpinejs(), sitemap(), tailwind(), partytown()],
+  integrations: [sitemap(), tailwind(), partytown(), vue()]
 });
---
+++ src/components/button.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Slot } from "radix-vue";
+</script>
+
+<template>
+  <Slot id="radix-01">
+  </Slot>
+</template>
